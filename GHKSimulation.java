bb746bb46e6aaccc327d75c35fb6e5fa3135ba03
ywkim17 <ywkim17@outlook.com>
2024-12-07 15:47:36 -0500

    changed the code so that I can retrieve values one by one

--- GHKSimulation.java
+++ GHKSimulation.java
@@ -11,31 +11,24 @@ public class GHKSimulation {
       
       // potassium parameter values
       double pK = 1;
-      double[] outsideK = {5,10,15,25,35,100,150,180,200};
-      double insideK = 210;
+      double outsideK = 0;
+      double insideK = 140;
       
       // sodium parameter values
-      double pNa = 0.075;
-      double outsideNa = 145;
-      double insideNa = 13;
+      double pNa = 0.00000001;
+      double outsideNa = 130;
+      double insideNa = 10;
       
       // chlorine parameter values
-      double pCl = 0.5;
+      double pCl = 1;
       double outsideCl = 130;
-      double insideCl = 13;
+      double insideCl = 10;
       
       // calculation
-      double[] V = new double[outsideK.length];
-      for(int i = 0; i < outsideK.length; i++) {
-         double currentOutsideK = outsideK[i];
-         double insideStuff = ((pK * currentOutsideK) + (pNa * outsideNa) + (pCl * insideCl)) /
+      double insideStuff = ((pK * outsideK) + (pNa * outsideNa) + (pCl * insideCl)) /
                               ((pK * insideK) + (pNa * insideNa) + (pCl * outsideCl));
-         V[i] = ((R * T) / F) * Math.log(insideStuff);
-      }
-      
-      for(int i = 0; i < V.length; i++) {
-         System.out.println(V[i]);
-      }
+      double mV = ((R * T) / F) * Math.log(insideStuff);
+      System.out.println(mV);
       
       stdin.close();
    }
